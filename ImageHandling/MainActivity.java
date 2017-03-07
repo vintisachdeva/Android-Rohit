ffaea8b605ae15e257b999931b2cac7da8b6d592
vintisachdeva <vintisachdeva17@gmail.com>
2017-03-07 14:19:55 +0530

    Add files via upload

---
+++ ImageHandling/MainActivity.java
@@ -0,0 +1,31 @@
+package com.bmpl.imagehandling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageResource(R.drawable.image2);
+            }
+        });
+    }
+}
