06559b2756ceb1e48bc3ef39533255252ac5ff03
vintisachdeva <vintisachdeva17@gmail.com>
2017-03-22 15:50:39 +0530

    Add files via upload

---
+++ Registration Form New/MainActivity.java
@@ -0,0 +1,46 @@
+package com.bmpl.registrationform;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText nameEditText, emailEditText, passwordEditText;
+
+    private final int requestCode = 10;
+
+    Button signIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameEditText = (EditText)findViewById(R.id.nameEditText);
+        emailEditText = (EditText)findViewById(R.id.emailEditText);
+        passwordEditText = (EditText)findViewById(R.id.passwordEditText);
+
+        signIn = (Button)findViewById(R.id.signInButton);
+
+        signIn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("name", nameEditText.getText().toString());
+                intent.putExtra("email", emailEditText.getText().toString());
+                intent.putExtra("password", passwordEditText.getText().toString());
+                nameEditText.setText("");
+                emailEditText.setText("");
+                passwordEditText.setText("");
+                startActivityForResult(intent, requestCode);
+                //startActivity(intent);
+            }
+        });
+    }
+}
